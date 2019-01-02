b39e4136d54857d285e4508f41e67680c5a67b5c
fugashy <fugashy@icloud.com>
2019-01-02 15:08:43 +0900

    Add conv and pooling layer

--- src/img_col_bridge.py
+++ src/img_col_bridge.py
@@ -7,20 +7,19 @@ class ImgColBridge:
     用いるフィルターに合わせて画像を展開する
     または、展開済みのデータを画像に戻す
     """
-    def __init__(self, filter_h, filter_w, shape, stride=1, pad=0):
+    def __init__(self, filter_h, filter_w, stride=1, pad=0):
         u"""
         Args:
             filter_h: フィルターの高さ(int)
             filter_w: フィルターの幅(int)
-            shape:    入出力するデータの形状(データ数、ch, height, width) (tuple)
             stride:   フィルター移動間隔(int)
             pad:      パディングパラメータ(int)
         """
-        self.__filter_h = filter_h
-        self.__filter_w = filter_w
-        self.__shape = shape
-        self.__stride = stride
-        self.__pad = pad
+        self.filter_h = filter_h
+        self.filter_w = filter_w
+        self.stride = stride
+        self.pad = pad
+        self.shape = None
 
     def to_col(self, data):
         u"""
@@ -29,45 +28,54 @@ class ImgColBridge:
         Args:
             data: 4Dデータ(データ数、ch, height, width)(numpy.array)
         """
-        N, C, H, W = self.__shape
-        out_h = (H + 2*self.__pad - self.__filter_h)//self.__stride + 1
-        out_w = (W + 2*self.__pad - self.__filter_w)//self.__stride + 1
+        N, C, H, W = data.shape
+        self.shape = data.shape
+
+        out_h = (H + 2*self.pad - self.filter_h)//self.stride + 1
+        out_w = (W + 2*self.pad - self.filter_w)//self.stride + 1
 
         img = np.pad(
                 data,
                 [(0,0), (0,0),
-                 (self.__pad, self.__pad),
-                 (self.__pad, self.__pad)],
+                 (self.pad, self.pad),
+                 (self.pad, self.pad)],
                  'constant')
-        col = np.zeros((N, C, self.__filter_h, self.__filter_w, out_h, out_w))
+        col = np.zeros((N, C, self.filter_h, self.filter_w, out_h, out_w))
 
-        for y in range(self.__filter_h):
-            y_max = y + (self.__stride * out_h)
-            for x in range(self.__filter_w):
-                x_max = x + (self.__stride * out_w)
+        for y in range(self.filter_h):
+            y_max = y + (self.stride * out_h)
+            for x in range(self.filter_w):
+                x_max = x + (self.stride * out_w)
                 col[:, :, y, x, :, :] = \
-                    img[:, :, y:y_max:self.__stride, x:x_max:self.__stride]
+                    img[:, :, y:y_max:self.stride, x:x_max:self.stride]
 
         col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
         return col
 
     def to_img(self, col):
-        N, C, H, W = self.__shape
-        out_h = (H + 2*self.__pad - self.__filter_h)//self.__stride + 1
-        out_w = (W + 2*self.__pad - self.__filter_w)//self.__stride + 1
+        if self.shape is None:
+            raise Exception('Data shape is required.')
+
+        N, C, H, W = self.shape
+        out_h = (H + 2*self.pad - self.filter_h)//self.stride + 1
+        out_w = (W + 2*self.pad - self.filter_w)//self.stride + 1
         col = col.reshape(
-                N, out_h, out_w, C, self.__filter_h, self.__filter_w).transpose(
+                N, out_h, out_w, C, self.filter_h, self.filter_w).transpose(
                         0, 3, 4, 5, 1, 2)
 
         img = np.zeros((
             N,
             C,
-            H + (2 * self.__pad) + self.__stride - 1,
-            W + (2 * self.__pad) + self.__stride - 1))
-        for y in range(self.__filter_h):
-            y_max = y + self.__stride*out_h
-            for x in range(self.__filter_w):
-                x_max = x + self.__stride*out_w
-                img[:, :, y:y_max:self.__stride, x:x_max:self.__stride] += col[:, :, y, x, :, :]
+            H + (2 * self.pad) + self.stride - 1,
+            W + (2 * self.pad) + self.stride - 1))
+        for y in range(self.filter_h):
+            y_max = y + self.stride*out_h
+            for x in range(self.filter_w):
+                x_max = x + self.stride*out_w
+                img[:, :, y:y_max:self.stride, x:x_max:self.stride] += col[:, :, y, x, :, :]
 
-        return img[:, :, self.__pad:H + self.__pad, self.__pad:W + self.__pad]
+        return img[:, :, self.pad:H + self.pad, self.pad:W + self.pad]
+
+    def to_img(self, col, shape):
+        self.shape = shape
+        return to_img(col)
--- src/layer.py
+++ src/layer.py
@@ -5,6 +5,7 @@ sys.path.append(os.pardir)
 import numpy as np
 import src.activation
 import src.loss
+import src.img_col_bridge
 
 u"""
 ニューラルネットワークを構成する様々な層の定義
@@ -378,3 +379,91 @@ class Dropout:
         desc['ratio'] = self.dropout_ratio
 
         return desc
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.bridge = img_col_bridge.ImgColBridge(W.shape[2], W.shape[3])
+
+        # 中間データ（backward時に使用）
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+        # 重み・バイアスパラメータの勾配
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, _, FH, FW = self.W.shape
+        N, _, H, W = x.shape
+
+        out_h = 1 + int((H + 2*self.bridge.pad - FH) / self.bridge.stride)
+        out_w = 1 + int((W + 2*self.bridge.pad - FW) / self.bridge.stride)
+
+        col = self.bridge.to_col(x)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = self.bridge.to_img(dcol)
+
+        return dx
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.bridge = img_col_bridge.ImgColBridge(self.pool_h, self.pool_w, stride, pad)
+
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = self.bridge.to_col(x)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = self.bridge.to_img(dcol)
+
+        return dx
