fbf6543b510f369e43ab5a673bc8dd1b6a5f4305
fugashy <fugashy@icloud.com>
2019-02-24 17:47:59 +0900

    Extract training process as class for clarification
    Training process does not depend on network in mnist.
    For centralization of the process, extract as class.

---
+++ src/viewer.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import sys, os
+import pathlib
+p = pathlib.Path(os.path.dirname(__file__) + '/..')
+root_dir_path = str(p.resolve())
+sys.path.append(root_dir_path)
+
+import src.plot as plot
+
+def create(config_dict, params):
+    acc_plotter = None
+    loss_plotter = None
+    param_plotter = None
+    if config_dict['acc']:
+        acc_plotter = plot.SimplePlotter(
+                'Accuracy', label=('epoch', 'accuracy[%]'), y_range=(-0.1, 1.1))
+    if config_dict['loss']:
+        loss_plotter = plot.SimplePlotter(
+                'Loss', label=('num', 'loss'), y_range=(-0.1, 1.1))
+    if config_dict['dnn_params']:
+        param_plotter = plot.NeuralNetworkParamVisualizer(params)
+
+    return Viewer(acc_plotter, loss_plotter, param_plotter)
+
+class Viewer:
+    def __init__(self, acc_plotter, loss_plotter, param_plotter):
+        self.__acc_plotter = acc_plotter
+        self.__loss_plotter = loss_plotter
+        self.__param_plotter = param_plotter
+
+    def view(self, train_acc_list, train_loss_list, test_acc_list):
+        if self.__param_plotter is not None:
+            self.__param_plotter.plot()
+
+        if self.__acc_plotter is not None:
+            self.__acc_plotter.plot([train_acc_list, test_acc_list])
+
+        if self.__loss_plotter is not None:
+            self.__loss_plotter.plot(train_loss_list)
